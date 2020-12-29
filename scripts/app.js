d874460e3b9e75313e89baba33a5b0691035ba01
Manish Ranjan <manishranjan2606@gmail.com>
2020-12-30 04:05:28 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -19,7 +19,7 @@ class UI {
   //submit budget form
   submitBudgetForm() {
     const value = this.budgetInput.value;
-    if (value === "" || value < 3) {
+    if (value === "" || value < 0) {
       this.budgetFeedback.classList.add("showItem");
       this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
 
